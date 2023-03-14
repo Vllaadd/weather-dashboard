0b17ba2185e3fd5f759996b2ba3558cf1d042a41
Vllaadd <vlladdiimiir@gmail.com>
2023-03-14 06:07:11 -0400

    trying to fetch the data

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,38 @@
-var apiKey = "166a433c57516f51dfab1f7edaed8413";
-var url = "https://api.openweathermap.org/data/2.5/forecast";
+$(document).ready(function(){
+    var apiKey = "166a433c57516f51dfab1f7edaed8413";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+    $(document).ready(function(){
+
+// click button 
+        $("#submit").click(function(){
+            getWeather();
+            var userInput = $("#user-input").val();
+        })
+
+
+// current forecast 
+            function getWeather(){
+                var city = $("#input").val();
+            $ajax({
+                url: url,
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    $("#city").html(data.name + "," + data.sys.country);
+                }
+            })
+            }
+            
+        
+    })
+
+
+   
+
+
+
+
+
+});
 
