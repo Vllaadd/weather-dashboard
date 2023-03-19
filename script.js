01c229447c3932c580168662f7c62d2931a9c1df
Vllaadd <vlladdiimiir@gmail.com>
2023-03-19 16:33:02 -0400

    its working url wasnt correct

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             function getWeather(){
                 var city = $("#input").val();
             $.ajax({
-                url: url,
+                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey,
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data){
@@ -22,8 +22,6 @@ $(document).ready(function(){
         };
 
             
-            var apiKey = "166a433c57516f51dfab1f7edaed8413";
-            var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
         
             
         
