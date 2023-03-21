d58a3323226f25babe7267b9d4ffc91b9e97bfe1
Vllaadd <vlladdiimiir@gmail.com>
2023-03-21 00:52:39 -0400

    five day forecast setting up

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var apiKey = "166a433c57516f51dfab1f7edaed8413";
+let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
 $(document).ready(function(){
     console.log("Document loaded.");
-// click button 
+// click button•••••••••••••••••••••••••••••••••••••••••••
         $("#submit").click(function(){
             getWeather();
+            fiveDayForecast();
         });
 
-// current forecast 
+// current forecast•••••••••••••••••••••••••••••••••••••••
             function getWeather(){
                 var city = $("#input").val();
             $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey,
+                url: url,
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data){
@@ -24,7 +26,19 @@ $(document).ready(function(){
             });
         };
 
-            
+// five day forecast•••••••••••••••••••••••••••••••••••••• 
+            function fiveDayForecast(){
+                $.ajax({
+                    url: url,
+                    dataType: "json",
+                    type: "GET",
+                    success: function(data){
+                        for(var i=0; i<data.list.length; i++){
+
+                        }
+                    }
+                })
+            }
         
             
         
